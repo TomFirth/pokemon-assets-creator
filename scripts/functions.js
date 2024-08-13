30dc2f7c9475671533f9c788169cee910f8551c9
TomFirth <tom.firth84@gmail.com>
2024-08-13 08:35:16 +0100

    save update and 3/4 padding

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,8 +1,13 @@
 pad = (n) => {
-  var len = 4 - ('' + n).length;
+  return (3 - ('' + n).length > 0 ? new Array(4 - ('' + n).length).join('0') : '') + n;
+}
+
+pad4 = (n) => {
+  const len = 4 - ('' + n).length;
   return (len > 0 ? new Array(++len).join('0') : '') + n;
 }
 
 module.exports = {
-  pad
+  pad,
+  pad4
 };
--- scripts/saveFile.js
+++ scripts/saveFile.js
@@ -1,18 +1,28 @@
 const fs = require("node:fs");
 const https = require("https");
-const config = require("../config");
+const config = require("../config.json");
 const functions = require("./functions.js");
 
-saveToDisk = (url, path) => {
-  let fullUrl = url;
-  let localPath = fs.createWriteStream(path);
-  https.get(fullUrl, (response) => {
+const saveToDisk = (url, path) => {
+  if (fs.existsSync(path)) {
+    console.log(`File already exists at ${path}, skipping download.`);
+    return;
+  }
+
+  const localPath = fs.createWriteStream(path);
+  https.get(url, (response) => {
     response.pipe(localPath);
-  })
+    console.log(`Downloaded and saved to ${path}`);
+  }).on('error', (error) => {
+    console.error(`Error downloading ${url}: ${error.message}`);
+  });
 }
 
 for (let i = 1; i <= config.numberOfPokemon; i++) {
-  let number = functions.pad(i);
-  saveToDisk(`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${number}.png`, `./images/raw/${number}.png`);
-  console.log(`${number} downloaded.`);
+  try {
+    let number = functions.pad(i);
+    saveToDisk(`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${number}.png`, `./images/raw/${number}.png`);
+  } catch (error) {
+    console.error(error);
+  }
 }
